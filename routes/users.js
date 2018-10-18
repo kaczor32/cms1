d800587cb8584e72268a5c393c2eaaead66df7db
Mateusz Kaczmarek <you@example.com>
2018-10-18 17:23:52 +0200

    login email to db

--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,7 @@ const bcrypt = require('bcryptjs');
 // load user model
 const User = require('../models/User')
 
-// route  GET posts
+// route  GET users
 // desc   Test users route
 // access Public
 
@@ -16,7 +16,7 @@ router.get('/', (req, res) => {
     });
 });
 
-// route  GET posts/register
+// route  GET users/register
 // desc   register user
 // access Public
 
@@ -55,5 +55,41 @@ router.post('/register', (req, res) => {
             }
         })
 });
+
+// route  GET users/login
+// desc   login user / returning JWTtoken
+// access Public
+
+router.post('/login', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    //find user by email
+    User.findOne({
+            email
+        })
+        .then(user => {
+            // check for user
+            if (!user) {
+                return res.status(404).json({
+                    email: 'user not found'
+                });
+            }
+            //check password
+            bcrypt.compare(password, user.password)
+                .then(isMatch => {
+                    if (isMatch) {
+                        res.json({
+                            msg: 'success'
+                        })
+                    } else {
+                        return res.status(400).json({
+                            password: 'Password incorrect'
+                        });
+                    };
+                });
+        });
+});
+
 
 module.exports = router;
