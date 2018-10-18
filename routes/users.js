2ad20b949eb1b95e174a5b0aed0f5decca230343
Mateusz Kaczmarek <you@example.com>
2018-10-18 19:13:54 +0200

    1 validation

--- routes/users.js
+++ routes/users.js
@@ -6,6 +6,9 @@ const jwt = require('jsonwebtoken');
 const keys = require('../config/keys');
 const passport = require('passport');
 
+//load input validation
+const validateRegisterInput = require('../validation/register');
+
 // load user model
 const User = require('../models/User')
 
@@ -24,14 +27,23 @@ router.get('/', (req, res) => {
 // access Public
 
 router.post('/register', (req, res) => {
+    const {
+        errors,
+        isValid
+    } = validateRegisterInput(req.body);
+
+    //check validation
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
+
     User.findOne({
             email: req.body.email
         })
         .then(user => {
             if (user) {
-                return res.status(400).json({
-                    email: 'email already exists'
-                });
+                errors.email = 'Email already exists'
+                return res.status(400).json(errors);
             } else {
                 const avatar = gravatar.url(req.body.email, {
                     s: '200',
